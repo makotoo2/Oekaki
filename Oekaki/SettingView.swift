780a242a436145799485470c7ada596e1bd7c639
makotoo2 <makotoo2@ca2.so-net.ne.jp>
2022-02-13 13:28:28 +0900

    Pickerでアイコンの色が設定できないため、色名を表示するようにした。

--- Oekaki/SettingView.swift
+++ Oekaki/SettingView.swift
@@ -14,28 +14,41 @@ struct SettingView: View {
     @Binding var lineWidth:Int
     @Binding var colors:[Color]
     
+    let cnames = ["黒", "赤", "青", "緑"]
     var body: some View {
         VStack{
             // 色選択
-            Picker(selection: $colorSel, label: Text("色").frame(width: 40)) {
-                ForEach(0..<colors.count){value in
-                    if value == self.colors.count - 1 {
-                        // 消しゴム用？
-                        Image(systemName: "square")                        
-                    } else {
-                        Image(systemName: "paintbrush.fill")
-                            .foregroundColor(self.colors[value])
+            HStack {
+                Text("色")
+                Picker(selection: $colorSel, label: Text("色").frame(width: 40)) {
+                    ForEach(0..<colors.count){value in
+                        if value == self.colors.count - 1 {
+                            HStack {
+                                Text("消しゴム")
+                                // 消しゴム用？
+                                Image(systemName: "square")
+                            }
+                        } else {
+                            HStack{
+                                Text(cnames[value])
+                                Image(systemName: "paintbrush.fill")
+                                    .foregroundColor(self.colors[value])
+                                //    .background(self.colors[value])
+                            }
+                        }
                     }
                 }
             }
-            
-            // 線幅選択
-            Picker(selection: $lineWidth, label: Text("線幅").frame(width: 40)) {
-                ForEach(1..<11){ value in
-                    Text(String(value))
+            HStack{
+                Text("線幅")
+                // 線幅選択
+                Picker(selection: $lineWidth, label: Text("線幅").frame(width: 40)) {
+                    ForEach(1..<11){ value in
+                        Text(String(value))
+                    }
                 }
+                .frame(width: 30)
             }
-            .frame(width: 30)
             
             Spacer()
             Button(action: {
